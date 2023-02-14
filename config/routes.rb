ebd2ebb7f4bb66a676904b2d2c88c1933fd952ac
Martim Correia <correia_martim@hotmail.com>
2023-02-14 16:26:43 +0000

    day one of linktree

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get 'dashboard', to: "dashboard#index"
+  get 'appearance', to: 'dashboard#appearance'
+  root 'dashboard#index'
+  # Allows us to use user_path(user) to get URL for a user's profile page
+  get ':id', to: 'dashboard#show', as: :user
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
